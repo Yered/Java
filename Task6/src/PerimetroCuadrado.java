996013ac888efbed9c91cf04b294ea74e026bbd7
anto <a.n_t.o@hotmail.com>
2017-05-24 09:52:49 +0100

    first commit

---
+++ Task6/src/PerimetroCuadrado.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+
+public class PerimetroCuadrado {
+
+	public static void main(String[] args) {
+		 Scanner teclado = new Scanner(System.in);
+		 int lado1,perimetro;
+		 System.out.print("introduzca el valor de un lado");
+		 lado1 = teclado.nextInt();
+		 perimetro = lado1*4;
+		 System.out.print("el perímetro del cuadraro es:" + perimetro);
+		 
+	}
+
+}

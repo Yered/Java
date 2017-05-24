1e0b0f5cab42dabb4d353e96993c348034505222
anto <a.n_t.o@hotmail.com>
2017-05-24 10:37:35 +0100

    Precio articulo

---
+++ Task6/src/PrecioArticulo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class PrecioArticulo {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int cantidad;
+		double precio;
+		System.out.println("Precio del articulo: ");
+		precio = teclado.nextDouble();
+		System.out.println("Cantidad de artículos: ");
+		cantidad = teclado.nextInt();
+		teclado.close();
+		System.out.println("La cantidad que se debe pagar es: " + precio*cantidad);
+	}
+
+}
